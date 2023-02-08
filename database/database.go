0595751abaeb550af3383470748781ea3af9c407
cora23tt <mikemcgrat11@gmail.com>
2023-02-08 14:12:39 +0500

    fully integrated with postgresql database

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -15,6 +16,10 @@ type Post struct {
 	AuthorID int64  `json:"author_id"`
 }
 
+type TagObj struct {
+	Tag string `json:"tag"`
+}
+
 type Dbinstance struct {
 	DB *sqlx.DB
 }
@@ -22,25 +27,15 @@ type Dbinstance struct {
 var Postgres Dbinstance
 
 func GetAll() ([]Post, error) {
-	posts := []Post{}
-
-	db, err := sqlx.Connect("postgres", "user=postgres password=3729 dbname=blogpost sslmode=disable") //
-	if err != nil {
-		log.Fatal("Failed to connect to database. \n", err)
-		os.Exit(2)
-	}
-
-	log.Println("DB connected")
-
-	rows, err := db.Query(`SELECT * FROM tbl_post;`)
+	rows, err := Postgres.DB.Query(`SELECT * FROM tbl_post;`)
 	if err != nil {
 		return nil, err
 	}
 
+	posts := []Post{}
 	for rows.Next() {
 		var post Post
-		err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID)
-		if err != nil {
+		if err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID); err != nil {
 			return nil, err
 		}
 		posts = append(posts, post)
@@ -50,22 +45,76 @@ func GetAll() ([]Post, error) {
 }
 
 func GetList(num int) ([]Post, error) {
-	posts := []Post{}
-
-	rows, err := Postgres.DB.Query(`SELECT * FROM tbl_post limit %d;`, num)
+	rows, err := Postgres.DB.Query(fmt.Sprintf(`SELECT * FROM tbl_post limit %d`, num))
 	if err != nil {
 		return nil, err
 	}
 
+	posts := []Post{}
 	for rows.Next() {
 		var post Post
-		err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID)
-		if err != nil {
+		if err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID); err != nil {
 			return nil, err
 		}
 		posts = append(posts, post)
 	}
 
+	return posts, nil
+}
+
+func GetPost(id int64) (Post, error) {
+	var post Post
+	rows, err := Postgres.DB.Query(fmt.Sprintf(`SELECT * FROM tbl_post WHERE id=%d LIMIT 1`, id))
+	if err != nil {
+		return Post{}, err
+	}
+
+	ok := rows.Next()
+	if ok {
+		if err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID); err != nil {
+			return Post{}, err
+		}
+	}
+	return post, nil
+}
+
+func AddPost(newpost Post) (int, error) {
+	var id int
+	query := fmt.Sprintf(`INSERT INTO tbl_post (title, content, author_id) VALUES ('%v', '%v', %d) RETURNING id`, newpost.Title, newpost.Content, newpost.AuthorID)
+	if err := Postgres.DB.QueryRow(query).Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func DeletePost(id int) error {
+	if _, err := Postgres.DB.Exec(`DELETE FROM tbl_post WHERE id=$1`, id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func EditPost(post Post) error {
+	if _, err := Postgres.DB.Exec(`UPDATE tbl_post SET title=$1, content=$2, author_id=$3 WHERE id=$4`, post.Title, post.Content, post.AuthorID, post.ID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetPostByTag(tag string) ([]Post, error) {
+	rows, err := Postgres.DB.Query(`select * from tbl_post where id in(select post_id from post_tag where tag_id in(select id from tbl_tag where name=$1))`, tag)
+	if err != nil {
+		return []Post{}, err
+	}
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		if err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.AuthorID); err != nil {
+			return []Post{}, err
+		}
+		posts = append(posts, post)
+	}
 	return posts, nil
 }
 
@@ -76,7 +125,7 @@ func ConnectDB() {
 		os.Exit(2)
 	}
 
-	log.Println("DB connected")
+	log.Println("DB connect success")
 
 	Postgres = Dbinstance{
 		DB: db,
--- main.go
+++ main.go
@@ -1,35 +1,37 @@
 package main
 
+// planning				DONE
+// Development			DONE
+// UnitTesting			PROGRESS
+// DocumentationSwaggo
+// Make Public in Git
+
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"testrepo/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.New()
+
+	r := gin.Default()
+	r.SetTrustedProxies([]string{"127.0.0.1"})
 	database.ConnectDB()
 
-	r.GET("/list", allPostsHandler) //GET ALL LIST
-	// r.GET("/list/:num", listPostsHandler) //GET LIST
+	r.GET("/list", allPostsHandler)       //GET ALL LIST
+	r.GET("/list/:num", listPostsHandler) //GET LIST
 
-	// r.GET("/post/:id", postHandler)          //GET POST
-	// r.POST("/post/", createPostHandler)      //CREATE POST
-	// r.DELETE("/post/:id", deletePostHandler) //DELETE POST
-	// r.POST("/post:id", editPostHandler)      //EDIT POST
+	r.GET("/post/:id", postHandler)          //GET POST
+	r.POST("/post/", createPostHandler)      //CREATE POST
+	r.DELETE("/post/:id", deletePostHandler) //DELETE POST
+	r.POST("/post/:id", editPostHandler)     //EDIT POST
 
-	// r.GET("/tag/:tag", postWithTag) //GET POST WITH TAG
+	r.POST("/bytag", postByTag) //POST POST BY TAG
 
-	r.Run()
-}
-
-type Post struct {
-	ID       int64  `json:"id"`
-	Title    string `json:"title"`
-	Content  string `json:"content"`
-	AuthorID int64  `json:"author_id"`
+	r.Run(":8080")
 }
 
 type User struct {
@@ -45,90 +47,101 @@ func allPostsHandler(c *gin.Context) {
 	if err != nil {
 		jsonValue, _ := json.Marshal(err)
 		c.JSON(http.StatusInternalServerError, jsonValue)
+		return
 	}
 	c.JSON(http.StatusOK, posts)
 }
 
-// func listPostsHandler(c *gin.Context) {
-// 	// s := c.Param("num")
-// 	c.JSON(http.StatusOK, Post{})
-// }
+func listPostsHandler(c *gin.Context) {
+	s := c.Param("num")
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var posts []database.Post
+	posts, err = database.GetList(num)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, posts)
+}
 
-// func postHandler(c *gin.Context) {
-// 	s := c.Param("id")
-// 	n, err := strconv.ParseInt(s, 10, 64)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
+func postHandler(c *gin.Context) {
+	s := c.Param("id")
+	n, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// 	for i, a := range posts {
-// 		if a.ID == n {
-// 			c.JSON(http.StatusOK, posts[i])
-// 			return
-// 		}
-// 	}
+	var post database.Post
+	post, err = database.GetPost(n)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+		return
+	}
 
-// 	c.Status(http.StatusNotFound)
-// }
+	c.JSON(http.StatusOK, post)
+}
 
-// func createPostHandler(c *gin.Context) {
-// 	newpostID := len(posts) + 1
+func createPostHandler(c *gin.Context) {
+	var newpost database.Post
+	if err := c.BindJSON(&newpost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// 	var newpost Post
-// 	if err := c.BindJSON(&newpost); err != nil {
-// 		c.Status(http.StatusBadRequest)
-// 		return
-// 	}
-// 	newpost.ID = int64(newpostID)
+	id, err := database.AddPost(newpost)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, id)
+}
 
-// 	posts = append(posts, newpost)
-// 	c.JSON(http.StatusOK, newpostID)
-// }
+func deletePostHandler(c *gin.Context) {
+	s := c.Param("id")
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	if err = database.DeletePost(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Status(http.StatusOK)
+}
 
-// func deletePostHandler(c *gin.Context) {
-// 	s := c.Param("id")
-// 	n, err := strconv.ParseInt(s, 10, 64)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func editPostHandler(c *gin.Context) {
+	var newpost database.Post
+	if err := c.BindJSON(&newpost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// 	for i, a := range posts {
-// 		if a.ID == n {
-// 			posts = append(posts[:i], posts[i+1:]...)
-// 		}
-// 	}
-// 	c.Status(http.StatusNoContent)
-// }
+	err := database.EditPost(newpost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-// func editPostHandler(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
 
-// 	s := c.Param("id")
-// 	n, err := strconv.ParseInt(s, 10, 64)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	for i, a := range posts {
-// 		if a.ID == n {
-
-// 			var post Post
-// 			if err := c.ShouldBindJSON(&post); err != nil {
-// 				c.JSON(http.StatusBadRequest, gin.H{
-// 					"error": err.Error(),
-// 				})
-// 				panic(err)
-// 			}
-
-// 			posts = append(posts[:i], posts[i+1:]...)
-// 			posts = append(posts, post)
-// 			c.Status(http.StatusOK)
-// 			return
-// 		}
-// 	}
-
-// 	c.Status(http.StatusBadRequest)
-// }
-
-// func postWithTag(c *gin.Context) {
-// }
+func postByTag(c *gin.Context) {
+	var tagObj database.TagObj
+	if err := c.BindJSON(&tagObj); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	var posts []database.Post
+	posts, err := database.GetPostByTag(tagObj.Tag)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, posts)
+}
